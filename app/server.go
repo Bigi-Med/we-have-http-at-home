58e9b76fe3a171854a9ee36dd17a0d64e67f3ce5
Bigi-Med <medbigi2000@gmail.com>
2024-12-03 21:39:14 +0100

    changes

--- app/server.go
+++ app/server.go
@@ -29,38 +29,48 @@ func main() {
             os.Exit(1)
         }
         go acceptCon(conn,args)
-     }
- }
- func acceptCon(conn net.Conn, file string){
-     data := make([]byte,1024)
-     conn.Read(data)
-     str := string(data)
-     strL := strings.Split(str, "\r\n")
-     strLineSplit := strings.Split(strL[0]," ")
-     strUserAgentValue := strings.Split(strL[2],":")
-     path := strLineSplit[1]
-     fmt.Printf("HTTP Request Path is : %s \n",path)
-     if path == "/" {
-         conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-     }else if strings.HasPrefix(path,"/echo"){
-         body := strings.Split(path, "/")
-         fmt.Printf("BODY MESSAGE IS %s \n",body[2])
-         conn.Write([]byte("HTTP/1.1 200 OK\r\nContent-type: text/plain\r\nContent-Length: 3\r\n\r\n"+body[2]))
-     } else if path == "/user-agent"{
-         fmt.Printf("USER AGENT HEADER %s \n",strUserAgentValue[1])
-         conn.Write([]byte("HTTP/1.1 200 OK\r\nContent-type: text/plain\r\nContent-Length: 11\r\n\r\n"+strUserAgentValue[1]))
-     } else if strings.HasPrefix(path,"/files"){
-         fmt.Print("Returning file content\n")
-         body := strings.Split(path, "/")
-         fPath := file+body[2]
-         fmt.Println(fPath)
-         data, err := os.ReadFile(fPath)
-         if err != nil {
-          conn.Write([]byte("HTTP/1.1 404 NOT FOUND\r\n\r\n"))  
-         }
-         length := len(data)
-         conn.Write([]byte("HTTP/1.1 200 OK\r\nContent-type: application/octet-stream\r\nContent-Length: "+strconv.Itoa(length)+"\r\n\r\n"+string(data)))
-     }else{
-         conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+    }
+}
+func acceptCon(conn net.Conn, file string){
+    data := make([]byte,1024)
+    conn.Read(data)
+    str := string(data)
+    strL := strings.Split(str, "\r\n")
+    strLineSplit := strings.Split(strL[0]," ")
+    strUserAgentValue := strings.Split(strL[2],":")
+    path := strLineSplit[1]
+    method := strLineSplit[0]
+    fmt.Println(method)
+    fmt.Printf("HTTP Request Path is : %s \n",path)
+    if method == "POST"{
+        fmt.Println(strL[7]) 
+        body := strings.Split(path, "/")
+        fileName := body[2]
+        os.WriteFile(file+fileName,[]byte(strL[7]),0644)
+    }else if method == "GET" {
+
+        if path == "/" {
+            conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+        }else if strings.HasPrefix(path,"/echo"){
+            body := strings.Split(path, "/")
+            fmt.Printf("BODY MESSAGE IS %s \n",body[2])
+            conn.Write([]byte("HTTP/1.1 200 OK\r\nContent-type: text/plain\r\nContent-Length: 3\r\n\r\n"+body[2]))
+        } else if path == "/user-agent"{
+            fmt.Printf("USER AGENT HEADER %s \n",strUserAgentValue[1])
+            conn.Write([]byte("HTTP/1.1 200 OK\r\nContent-type: text/plain\r\nContent-Length: 11\r\n\r\n"+strUserAgentValue[1]))
+        } else if strings.HasPrefix(path,"/files"){
+            fmt.Print("Returning file content\n")
+            body := strings.Split(path, "/")
+            fPath := file+body[2]
+            fmt.Println(fPath)
+            data, err := os.ReadFile(fPath)
+            if err != nil {
+                conn.Write([]byte("HTTP/1.1 404 NOT FOUND\r\n\r\n"))  
+            }
+            length := len(data)
+            conn.Write([]byte("HTTP/1.1 200 OK\r\nContent-Length: "+strconv.Itoa(length)+"\r\n\r\n"+string(data)))
+        }else{
+            conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+        }
      }
  }
